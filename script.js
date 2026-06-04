d69ec164a7a3843fd7f3dfc9b6f8a8bd7ff1e263
christianolhasso30029-web <christian.olhasso30029@eliteacademic.com>
2026-06-04 16:32:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = docunment.getElementById("status");
+const restartBtn = docunment.getElementById("restart")
 
+let currentPlayer = "X";
+let gameActive = true;
+let board = ["", "" ,"" ,"" ,"" , "", "", "", ""];
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+  ];
+
+function handleCellClick() {
+  const index = this.dataset.index;
+
+  if (board[index] !== "" || !gameActive) {
+    return;
+  }
